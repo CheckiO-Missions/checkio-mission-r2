19960a53b700861b1cbb2385d6735ce34002635e
Глеб <147049192+gleb734@users.noreply.github.com>
2023-12-13 20:00:09 +0300

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -43,24 +43,59 @@ TESTS = {
     ],
     "Extra": [
         {
-            "input": [['p1', 'p1', 'p1', 'p3', 'p4']],
+            "input": [['p1', 'p1', 'p1', 'p1', 'm2', 'm2', 'm6', 'm6', 's3', 's3', 's6', 's6', 's7', 's7']],
             "answer": False,
             "explanation": 'Two identical pairs in the chitoisu...'
         },
         {
-            "input": [['p1', 'p1', 'p1', 'p3', 'p4']],
-            "answer": False,
+            "input": [['p1', 'p9', 's1', 's9', 'm1', 'm9', 'dw', 'dg', 'dr', 'ww', 'we', 'ws', 'wn', 's9'],
+            "answer": True,
             "explanation": 'Kokushi-muso'
         },
         {
-            "input": [['p1', 'p1', 'p1', 'p3', 'p4']],
+            "input": [['p1', 'p9', 's1', 's9', 'm1', 'm9', 'ww', 'we', 'ws', 'wn'], ['dwrg']],
             "answer": False,
             "explanation": 'No, this is not kokushi'
         },
         {
-            "input": [['p1', 'p1', 'p1', 'p3', 'p4']],
+            "input": [['p1', 'p1', 'p1', 'p2', 'p2', 'p2', 'p3', 'p3', 'p3', 'p4', 'p4', 'p4', 'p5', 'p5']],
+            "answer": True,
+            "explanation": 'How many ways are there to break this hand into sets?...'
+        },
+        {
+            "input": [['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 'p1', 'p1']['weeg']],
             "answer": False,
-            "explanation": 'Incorrect number of arguments'
+            "explanation": 'Incorrect open set. Absolutely incorrect.'
+        },
+        {
+            "input": [['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 'p1', 'p1'], ['p222']],
+            "answer": True,
+            "explanation": 'Correct open set.'
+        },
+        {
+            "input": [['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 'p1', 'p1'], ['p111']],
+            "answer": False,
+            "explanation": 'Two many same tiles.'
+        },
+        {
+            "input": [['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 'p1', 'p1'], ['p1111']],
+            "answer": False,
+            "explanation": 'Incorrect open set 2'
+        },
+        {
+            "input": [['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 'p1', 'p1'], ['p123']],
+            "answer": True,
+            "explanation": 'Correct open set 2...'
+        },
+        {
+            "input": [['s1', 's2', 's3', 's4', 's5', 's6', 'p1', 'p1'], ['p123', 's789']],
+            "answer": True,
+            "explanation": 'Several open sets'
+        },
+        {
+            "input": [['s1', 's2', 's3', 's4', 's5', 's6', 'p1', 'p1'], ['p123', 's781']],
+            "answer": True,
+            "explanation": 'Incorrect several open sets'
         }
     ]
 }
